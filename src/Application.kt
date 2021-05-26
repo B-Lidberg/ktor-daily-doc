a40f9af4e5d9feff09e4e9f1535b84f371a963a2
B-Lidberg <76635047+B-Lidberg@users.noreply.github.com>
2021-05-26 18:11:13 -0400

    ktor install features implemented

---
+++ src/Application.kt
@@ -0,0 +1,24 @@
+package com.lid
+
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.gson.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(Routing)
+    install(ContentNegotiation) {
+        gson {
+            setPrettyPrinting()
+        }
+    }
+}
+
